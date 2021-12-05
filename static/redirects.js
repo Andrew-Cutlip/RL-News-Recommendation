ff72ece3166c002a0ed779571d4925f53d13eb4c
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-05 02:57:20 -0500

    Reformats click function

--- static/redirects.js
+++ static/redirects.js
@@ -1,7 +1,7 @@
 
 let track = (link, id) => {
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "/click"+ id );
+    xhr.open("POST", "/click/"+ id );
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify({
             "link": link.dataset.track
