85e086c4d024668bdb094ccd05b4b048dcb6ecb5
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-05 05:31:05 -0500

    Adds ratings

--- static/redirects.js
+++ static/redirects.js
@@ -8,9 +8,17 @@ let track = (link, id) => {
         }));
 }
 
-let rate = (link, rating) => {
+let ratings = document.getElementsByName("star");
+
+let rate = (link) => {
+    let rating = 3;
+    for (let i = 0; i < ratings.length; i++) {
+        if (ratings[i].checked) {
+            rating = ratings[i].value ;
+        }
+    }
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "/click/"+ rating );
+    xhr.open("POST", "/rate/"+ rating );
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify({
             "link": link.dataset.track
