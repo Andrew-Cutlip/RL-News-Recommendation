90871ef916b6bb922480c8dd85de4d9d4f0d1e8f
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-06 12:44:21 -0500

    Prints(inputs)

--- recommend.py
+++ recommend.py
@@ -116,6 +116,7 @@ def get_best_n_articles(values: list, n: int):
 
 def make_recommendation(last_clicks: list):
     inputs = get_inputs(last_clicks)
+    print(inputs)
     # need to flatten inputs
     inputs = np.array(inputs)
     inputs = inputs.flatten()
