adec891637e1daac25fbe5cbe095fff5024e87ab
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-06 11:40:26 -0500

    Completes training function

--- reccomend.py
+++ reccomend.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 import reward
 import tensorflow as tf
+from keras import backend as k
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
@@ -17,7 +18,8 @@ config = {
     "input_size": 80,
     "output_size": total_num_articles,
     "alpha": 0.01,
-    "recommendation_size": 10
+    "recommendation_size": 10,
+    "gamma": 0.99
 }
 
 
@@ -39,23 +41,35 @@ def create_actor(input_shape, output_shape, lr) -> Sequential:
     actor.add(Dense(128, activation="relu"))
     actor.add(Dense(64, activation="relu"))
     actor.add(Dense(output_shape, activation="Softmax", dtype="float64"))
-    actor.compile(optimizer=Adam(lr), loss="mse")
+    actor.compile(optimizer=Adam(lr), loss=actor_loss)
 
     return actor
+
+
+def actor_loss(advantages, actions):
+    clipped_act = k.clip(actions, 1e-8, 1 - 1e-8)
+    # print(advantages.shape)
+    # print(clipped_act.shape)
+    losses = (- clipped_act * advantages)
+    loss = tf.math.reduce_mean(losses)
+    return loss
 
 
 def create_critic(input_shape, output_shape, lr) -> Sequential:
     critic = Sequential()
     critic.add(tf.keras.Input(shape=(input_shape ,)))
-    critic.add(Dense(128 , activation='relu'))  # Hidden layer.
+    critic.add(Dense(128, activation='relu'))  # Hidden layer.
     critic.add(Dense(64, activation='relu'))  # Hidden layer.
     critic.add(Dense(output_shape , activation='linear'))  # Output layer.
     critic.compile(optimizer=Adam(learning_rate=lr), loss="mse")
     return critic
 
 
-model = deep_q_network(config["input_size"], config["output_size"], config["alpha"])
+# model = deep_q_network(config["input_size"], config["output_size"], config["alpha"])
 
+actor = create_actor(config["input_size"], config["output_size"], config["alpha"])
+critic = create_critic(config["input_size"], 1, config["alpha"])
+optimizer = Adam(config["alpha"])
 
 # will want to initialize model based on saved weights on server start
 def load_model():
@@ -86,7 +100,7 @@ def get_inputs(clicks: list):
         else:
             rating = 0
 
-        input_c = (click_id , art_id , cat_id , client_id , clicked , rated , list_number , rating)
+        input_c = (click_id, art_id, cat_id, client_id, clicked, rated, list_number, rating)
         inputs.append(input_c)
 
     return inputs
@@ -94,7 +108,7 @@ def get_inputs(clicks: list):
 
 def get_best_n_articles(values: list, n: int):
     # get indices of n largest values
-    indices = np.argpartition(values, - n)[- n :]
+    indices = np.argpartition(values, - n)[- n:]
 
     # these will be the article ids
     return indices
@@ -103,10 +117,14 @@ def get_best_n_articles(values: list, n: int):
 def make_recommendation(last_clicks: list):
     inputs = get_inputs(last_clicks)
     # get values from model
-    action_vals = model.predict(inputs)
+    # action_vals = model.predict(inputs)
+    action_vals = actor.predict(inputs)
     # will probably need to extract article ids somehow
-    best_articles = get_best_n_articles(action_vals, config["recommendation_size"])
+    best_article_ids = get_best_n_articles(action_vals, config["recommendation_size"])
     # need to get articles from db and return them
+    best_articles = db.articles_by_ids(best_article_ids)
+
+    return best_articles, best_article_ids, action_vals
 
 
 def random_articles(n: int):
@@ -160,13 +178,46 @@ def train_model():
     buff = np.array(replay_buffer)
     episodes = config["num_episodes"]
     batch_size = config["batch_size"]
+    all_rewards = []
     for e in range(episodes):
         # sample replay buffer
         indices = np.random.choice(len(replay_buffer), batch_size, replace=False)
         batch = buff[indices]
-        for sample in batch:
-            client_id = sample[0][2]
-            recommend = sample[2]
-            # need to get actual clicks from click_ids
-            reward_val = reward.calculate_reward(client_id, recommend)
-            print(reward_val)
+        with tf.GradientTape() as tape:
+            values = tf.zeros(batch_size)
+            all_probs = tf.zeros(batch_size)
+            advantages = tf.zeros(batch_size)
+            rewards = tf.zeros(batch_size)
+            for i, sample in enumerate(batch):
+                client_id = sample[0][2]
+                recommend = sample[2]
+                state = sample[0]
+                value = critic.predict(state)
+                values[i] = value
+                next_value = critic.predict(recommend)
+                # need to get actual clicks from click_ids
+                reward_val = reward.calculate_reward(client_id, recommend)
+                print(reward_val)
+                rewards[i] = reward_val
+                target = reward_val + config["gamma"] * next_value
+                advantage = target - value
+                advantages[i] = advantage
+
+                probs = sample[1]
+
+                log_probs = tf.math.log(probs)
+                all_probs[i] = log_probs
+
+            critic.fit(values, advantages)
+
+            loss = actor_loss(advantages, all_probs)
+            print(loss)
+
+            # need to update actor weights too
+            a_vars = actor.trainable_variables
+            a_grad = tape.gradient(loss, a_vars)
+            optimizer.apply_gradients(zip(a_grad, a_vars))
+
+            all_rewards.append(rewards)
+
+    return all_rewards
