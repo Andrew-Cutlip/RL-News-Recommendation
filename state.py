b1f675a6e3a50abaa94fd42e022203c3a0484691
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-07 13:05:39 -0500

    Gets last clicks right

--- state.py
+++ state.py
@@ -11,7 +11,12 @@ def get_last_n_click(n: int, clicks: list) -> list:
         clicks = db.get_all_clicks()
         clicks = np.array(clicks)
         random = clicks[np.random.choice(len(clicks), num_left, replace=False)]
-        ret = clicks + random.tolist()
+        print("Random clicks")
+        print(random)
+        if len(clicks) > 0:
+            ret = clicks + random.tolist()
+        else:
+            ret = random.tolist()
         return ret
 
     else:
