5bb3bf3c74163f8db8b8d810b5ecb97f1a214da1
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-07 13:15:23 -0500

    Flushes statements

--- state.py
+++ state.py
@@ -11,8 +11,10 @@ def get_last_n_click(n: int, clicks: list) -> list:
         clicks = db.get_all_clicks()
         clicks = np.array(clicks)
         random = clicks[np.random.choice(len(clicks), num_left, replace=False)]
-        print("Random clicks")
-        print(random)
+        print("Random clicks", flush=True)
+        print(random, flush=True)
+        print("Clicks", flush=True)
+        print(clicks, flush=True)
         if len(clicks) > 0:
             ret = clicks + random.tolist()
         else:
