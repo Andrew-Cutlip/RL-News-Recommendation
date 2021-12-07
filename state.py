8930c0dc90a1c9945c2ac7630667eb94a6114e29
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-07 15:21:53 -0500

    Always gets random clicks

--- state.py
+++ state.py
@@ -6,19 +6,15 @@ def get_last_n_click(n: int, clicks: list) -> list:
     # need to supplement data
     if len(clicks) < n:
         print("Getting random clicks")
-        num_left = n - len(clicks)
         # get random clicks for the rest
         clicks = db.get_all_clicks()
         clicks = np.array(clicks)
-        random = clicks[np.random.choice(len(clicks), num_left, replace=False)]
+        random = clicks[np.random.choice(len(clicks), n, replace=False)]
         print("Random clicks", flush=True)
         print(random, flush=True)
         print("Clicks", flush=True)
         print(clicks, flush=True)
-        if len(clicks) > 0:
-            ret = np.concatenate((clicks, random), axis=0)
-        else:
-            ret = random.tolist()
+        ret = random.tolist()
         return ret
 
     else:
