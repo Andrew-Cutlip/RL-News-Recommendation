99a765cc50b2721c76de3744b6ca59aff4340163
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-07 12:39:53 -0500

    Printing random clicks

--- state.py
+++ state.py
@@ -5,6 +5,7 @@ import numpy as np
 def get_last_n_click(n: int, clicks: list) -> list:
     # need to supplement data
     if len(clicks) < n:
+        print("Getting random clicks")
         num_left = n - len(clicks)
         # get random clicks for the rest
         clicks = db.get_all_clicks()
