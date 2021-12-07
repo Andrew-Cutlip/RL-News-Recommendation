9f55f58b311047c950ae0162665aa177e22d975f
Andrew Cutlip <snakeeye10@gmail.com>
2021-12-07 07:09:48 -0500

    Gets random clicks for any missing

--- state.py
+++ state.py
@@ -1,9 +1,17 @@
+import db.db as db
+import numpy as np
+
 
 def get_last_n_click(n: int, clicks: list) -> list:
     # need to supplement data
     if len(clicks) < n:
         num_left = n - len(clicks)
         # get random clicks for the rest
+        clicks = db.get_all_clicks()
+        clicks = np.array(clicks)
+        random = clicks[np.random.choice(len(clicks), num_left, replace=False)]
+        ret = clicks + random
+        return ret
 
     else:
         last_clicks: list = clicks[0:n]
